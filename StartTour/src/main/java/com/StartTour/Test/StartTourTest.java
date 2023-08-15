ea1c1426db13c7459e68b74ca578068af1a3f0b1
ankitaranithakur <142123531+ankitaranithakur@users.noreply.github.com>
2023-08-15 22:29:20 +0100

    nightcommit

---
+++ StartTour/src/main/java/com/StartTour/Test/StartTourTest.java
@@ -0,0 +1,16 @@
+package com.StartTour.Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class StartTourTest {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.startours.co.uk/");
+}
+
+}
